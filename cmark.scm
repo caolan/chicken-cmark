95f5398703afa5f7bd7394fe1c76409705d7a119
Caolan McMahon <caolan@caolanmcmahon.com>
2015-08-21 11:41:41 +0100

    add support for safe option

--- cmark.scm
+++ cmark.scm
@@ -6,13 +6,14 @@
 (import scheme chicken foreign)
 (foreign-declare "#include \"cmark.h\"")
 
-(define CMARK_OPT_DEFAULT
-  (foreign-value "CMARK_OPT_DEFAULT" int))
+(define CMARK_OPT_DEFAULT (foreign-value "CMARK_OPT_DEFAULT" int))
+(define CMARK_OPT_SAFE (foreign-value "CMARK_OPT_SAFE" int))
 
 (define cmark-markdown-to-html
-  (foreign-lambda c-string "cmark_markdown_to_html" c-string int int))
+  (foreign-lambda c-string "cmark_markdown_to_html" c-string size_t int))
 
-(define (commonmark->html input)
-  (cmark-markdown-to-html input (string-length input) CMARK_OPT_DEFAULT))
+(define (commonmark->html input #!key (safe #t))
+  (let ((opt (if safe CMARK_OPT_SAFE CMARK_OPT_DEFAULT)))
+    (cmark-markdown-to-html input (string-length input) opt)))
 
 )
